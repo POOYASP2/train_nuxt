46bdcdb132a784b1ae177abbff39b7ce70438466
pooya sharifi <pooyasharifi.sp2@gmail.com>
2021-12-05 23:37:04 +0330

    🚧 add user slider

---
+++ components/UserCart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <NuxtLink class="userCart" to="/">
+      <h1>test</h1>
+      <p>tetst</p>
+    </NuxtLink>
+    <!-- <img src="" alt=""> -->
+    <!-- <p></p>
+      <a href="http://"></a>
+      <a href="mailto:"></a> -->
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.userCart {
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+}
+/* .userCart :hover {
+  color: #6c55f9;
+  font-weight: bold;
+} */
+.userCart :visited {
+  color: inherit;
+}
+</style>
