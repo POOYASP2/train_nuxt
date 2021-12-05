9a79d15775d8c4c0f15ff4fdfae76c782358efc9
pooya sharifi <pooyasharifi.sp2@gmail.com>
2021-12-05 23:33:11 +0330

    ➕ add header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,77 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
-</template>
+  <main>
+    <section class="navbar">
+      <div class="navbar_cont">
+        <div class="navbar_logo">
+          <h2><span style="color: #6c55f9">فروشگاه </span>تست</h2>
+        </div>
+        <div></div>
+        <div class="navbar_items">
+          <NuxtLink
+            v-for="link in links"
+            :key="link.id"
+            :to="{ name: link.id }"
+            :class="{ active: $nuxt.$route.name == link.id }"
+            >{{ link.name }}</NuxtLink
+          >
+        </div>
+      </div>
+    </section>
+    <Nuxt />
+    <!-- <footer>
 
+    </footer> -->
+  </main>
+</template>
 <script>
+import { mapState } from 'vuex'
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+  computed: {
+    ...mapState(['links']),
+  },
 }
 </script>
+<style scoped>
+.navbar {
+  width: 100%;
+  padding: 1em 2em;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+}
+.navbar_cont {
+  display: flex;
+  align-items: center;
+  max-width: 950px;
+}
+.navbar_logo h2 {
+  margin: 0;
+  flex-grow: 1;
+}
+.navbar_items {
+  display: flex;
+  list-style: none;
+  justify-content: space-around;
+  flex-grow: 2;
+  padding-left: 15%;
+}
+.navbar a {
+  transition: color 0.25s ease-in;
+}
+.navbar a:link {
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+}
+.navbar a:hover {
+  color: #6c55f9;
+  font-weight: bold;
+}
+a:visited {
+  color: inherit;
+}
+.active {
+  color: #6c55f9 !important;
+  font-family: 'VazirB';
+  font-weight: bold;
+}
+</style>
